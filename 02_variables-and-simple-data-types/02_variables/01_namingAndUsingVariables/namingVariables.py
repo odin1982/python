9f0b27fc6d12f7614cdd62227a395cd166cf89d7
Odin Araujo <odin.araujo.barragan@gmail.com>
2026-04-08 20:08:09 -0600

    variables and simple data types - variables - naming and using variables

---
+++ 02_variables-and-simple-data-types/02_variables/01_namingAndUsingVariables/namingVariables.py
@@ -0,0 +1,9 @@
+name = "hola"
+last_name = "araujo"
+odin2 = "odin"
+_empresa = "bbva";
+# $moneda = "dolar" # DOES NOT COMPILE
+# 2manzanas ="manzana" #DOES NOT COMPILE
+
+print("nombre: " + name)
+print("empresa: " + _empresa)
