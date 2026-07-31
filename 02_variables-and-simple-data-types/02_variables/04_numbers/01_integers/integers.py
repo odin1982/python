7a14f599be09907e1a872794735b6d769fccddb3
odin <odin.araujo.barragan@gmail.com>
2026-07-31 14:07:41 -0600

    variables and simple data types - variables - numbers - integers

---
+++ 02_variables-and-simple-data-types/02_variables/04_numbers/01_integers/integers.py
@@ -0,0 +1,20 @@
+result = 2+3
+print(result)
+
+result = 3-2
+print(result)
+
+result = 2*3
+print(result)
+
+result = 2/3
+print(result)
+
+# exponent
+result = 3**2
+print(result)
+
+result = 2+3*4
+print(result)
+print("2+3*4 =",result)
+print("2+3*4 = " + str(result))
