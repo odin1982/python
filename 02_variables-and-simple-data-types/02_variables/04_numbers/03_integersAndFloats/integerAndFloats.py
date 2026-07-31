32fc4d1c9cca81dd2a07356b19a1cf7e4eba6e98
odin <odin.araujo.barragan@gmail.com>
2026-07-31 14:09:29 -0600

    variables and simple data types - variables - numbers - integers and floats

---
+++ 02_variables-and-simple-data-types/02_variables/04_numbers/03_integersAndFloats/integerAndFloats.py
@@ -0,0 +1,11 @@
+result = 4/2
+print("4/2 = ",result)
+
+result = 1 + 2.0
+print("1 + 2.0 = ",result)
+
+result = 2 * 3.0
+print("2 * 3.0 = ",result)
+
+result = 3.0 ** 2
+print("3.0 ** 2 = ",result)
