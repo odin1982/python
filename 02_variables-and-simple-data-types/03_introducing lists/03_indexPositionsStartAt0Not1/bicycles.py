7999b9a2799c23186dd49b2f85879e5c40d81698
odin <odin.araujo.barragan@gmail.com>
2026-08-06 20:56:20 -0600

    introducing list - index positions start at 0 not 1

---
+++ 02_variables-and-simple-data-types/03_introducing lists/03_indexPositionsStartAt0Not1/bicycles.py
@@ -0,0 +1,6 @@
+bicycles = ['trek','cannondale','redline','specialized']
+print(bicycles)
+print(bicycles[1])
+print(bicycles[3])
+print(bicycles[-1]) # te trae el ultimo elemento de la lista
+print(bicycles[-2]) # te trae el penultimo elemento de la lista
