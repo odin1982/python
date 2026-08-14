87a784bf4fb7d23292926aa7b9b69b6d1305750d
odin <odin.araujo.barragan@gmail.com>
2026-08-13 22:28:42 -0600

    introducing list - try it yourself

---
+++ 02_variables-and-simple-data-types/03_introducing lists/05_tryItYourself/tryItYourSelf.py
@@ -0,0 +1,8 @@
+names = ["Michael", "Bob", "Tracy"]
+print(names[0])
+print(names[1])
+print(names[2])
+
+print(f"Thank you for being here {names[0]}.")
+print(f"Thank you for being here {names[1]}.")
+print(f"Thank you for being here {names[2]}.")
